387f2ce0da863841d4b3d7e7d0a4e81160e729c2
YangJiao85 <42176196+YangJiao85@users.noreply.github.com>
2020-02-11 22:05:10 +0100

    Add files via upload

---
+++ C11_Capstone/t3_FeatureSelection/tol-0.97/3FeatureSelection.py
@@ -0,0 +1,89 @@
+import pandas as pd
+from sklearn import preprocessing
+import sklearn.model_selection as ms
+from sklearn import linear_model
+import sklearn.metrics as sklm
+from sklearn import feature_selection as fs
+from sklearn import metrics
+import numpy as np
+import numpy.random as nr
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy.stats as ss
+import math
+import sys
+np.set_printoptions(threshold=sys.maxsize)
+
+ddir = '../'
+Feature_names = np.array(pd.read_csv(ddir + 'hmda_features.csv').columns)
+Features = np.array(pd.read_csv(ddir + 'hmda_features.csv'))
+Labels = np.array(pd.read_csv(ddir + 'hmda_labels.csv'))
+print('Shape of Fetures and Labels')
+print(Features.shape)
+print(Labels.shape)
+print(Feature_names)
+
+## Eliminate low variance features
+##   using a p = 0.95 threshold
+## Define the variance threshold and fit the threshold to the feature array.
+sel = fs.VarianceThreshold(threshold = (.97 * (1. - .97)))
+Features_reduced = sel.fit_transform(Features)
+
+## Print the support and shape for the transformed features
+print(sel.get_support())
+print(Features_reduced.shape)
+Feature_names_red = Feature_names[sel.get_support()]
+print(Feature_names_red)
+
+## Feature selection for test set
+Features_new = np.array(pd.read_csv(ddir + 'hmda_features_new.csv'))
+print('Shape of Fetures for new dataset')
+print(Features_new.shape)
+Features_new_reduced = Features_new[:, sel.get_support()]
+print(Features_new_reduced.shape)
+
+## Select k best features
+## Reshape the Label array
+Labels = Labels.reshape(Labels.shape[0],)
+
+## Set folds for nested cross validation
+nr.seed(988)
+feature_folds = ms.KFold(n_splits=10, shuffle = True)
+
+## Define the model
+lin_mod = linear_model.Ridge()
+
+## Perform feature selection by CV with high variance features only
+nr.seed(6677)
+selector = fs.RFECV(estimator = lin_mod, cv = feature_folds, scoring = 'r2')
+selector = selector.fit(Features_reduced, Labels)
+print(selector.support_)
+print(selector.ranking_)
+print(Feature_names_red[selector.get_support()])
+
+## Apply the selector to the feature array
+Features_reduced = selector.transform(Features_reduced)
+print(Features_reduced.shape)
+
+## Feature selection for test set
+print('Shape of Fetures for new dataset')
+Features_new_reduced = Features_new_reduced[:, selector.get_support()]
+print(Features_new_reduced.shape)
+
+## Save selected features
+hmda_features_sel = pd.DataFrame(Features_reduced)
+hmda_features_sel.to_csv('hmda_features_red.csv', index=False)
+hmda_features_new_sel = pd.DataFrame(Features_new_reduced)
+hmda_features_new_sel.to_csv('hmda_features_new_red.csv', index=False)
+
+## r2 plot vs. the number of features
+def plot_r2_nf(selector):
+    plt.plot(range(1, len(selector.grid_scores_) + 1), selector.grid_scores_)
+    plt.title(r'Mean $R^2$ by number of features')
+    plt.ylabel(r'$R^2$')
+    plt.xlabel('Number of features')
+    plt.show()
+
+plot_r2_nf(selector)
+
+
---
+++ C11_Capstone/t3_FeatureSelection/tol-0.97/4Modeling.py
@@ -0,0 +1,101 @@
+## Construct model
+## Label: rate_spread
+## 
+import pandas as pd
+from sklearn import preprocessing
+import sklearn.model_selection as ms
+from sklearn import linear_model
+import sklearn.metrics as sklm
+import numpy as np
+import numpy.random as nr
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy.stats as ss
+import math
+
+# Import data
+Features = np.array(pd.read_csv('hmda_features_red.csv'))
+Labels = np.array(pd.read_csv('hmda_labels.csv'))
+print('Load Features and Labels')
+print(Features.shape)
+print(Labels.shape)
+
+## Split the dataset
+nr.seed(1234)
+#labels = np.array(hmda_train['rate_spread'])
+indx = range(Features.shape[0])
+indx = ms.train_test_split(indx, test_size = 20000)
+X_train = Features[indx[0],:]
+y_train = np.ravel(Labels[indx[0]])
+X_test = Features[indx[1],:]
+y_test = np.ravel(Labels[indx[1]])
+print(X_train.shape)
+print(X_test.shape)
+
+## Construct the linear regression model
+##   Define and fit the linear regression model
+lin_mod = linear_model.LinearRegression(fit_intercept = False)
+lin_mod.fit(X_train, y_train)
+
+print(lin_mod.intercept_)
+print(lin_mod.coef_)
+
+## Evaluate the model
+def print_metrics(y_true, y_predicted, n_parameters):
+    ## First compute R^2 and the adjusted R^2
+    r2 = sklm.r2_score(y_true, y_predicted)
+    r2_adj = r2 - (n_parameters - 1)/(y_true.shape[0] - n_parameters) * (1 - r2)
+
+    ## Print the usual metrics and the R^2 values
+    print('Mean Square Error      = ' + str(sklm.mean_squared_error(y_true, y_predicted)))
+    print('Root Mean Square Error = ' + str(math.sqrt(sklm.mean_squared_error(y_true, y_predicted))))
+    print('Mean Absolute Error    = ' + str(sklm.mean_absolute_error(y_true, y_predicted)))
+    print('Median Absolute Error  = ' + str(sklm.median_absolute_error(y_true, y_predicted)))
+    print('R^2                    = ' + str(r2))
+    print('Adjusted R^2           = ' + str(r2_adj))
+
+y_score = lin_mod.predict(X_test)
+y_score = y_score.round(1)
+print_metrics(y_test, y_score, 500)
+
+##   Desplay the residuals  
+def resid_plot(y_test, y_score):
+    ## first compute vector of residuals.
+    resids = np.subtract(y_test.reshape(-1,1), y_score.reshape(-1,1))
+    ## now make the residual plots
+    sns.regplot(y_score, resids, fit_reg=False)
+    plt.title('Residuals vs. predicted values')
+    plt.xlabel('Predicted values')
+    plt.ylabel('Residual')
+    plt.show()
+
+def hist_resids(y_test, y_score):
+    ## first compute vector of residuals. 
+    resids = np.subtract(y_test.reshape(-1,1), y_score.reshape(-1,1))
+    ## now make the residual plots
+    sns.distplot(resids)
+    plt.title('Histogram of residuals')
+    plt.xlabel('Residual value')
+    plt.ylabel('count')
+    plt.show()
+
+def resid_qq(y_test, y_score):
+    ## first compute vector of residuals.
+    resids = np.subtract(y_test, y_score)
+    ## now make the residual plots
+    ss.probplot(resids.flatten(), plot = plt)
+    plt.title('Residuals vs. predicted values')
+    plt.xlabel('Predicted values')
+    plt.ylabel('Residual')
+    plt.show()
+
+
+hist_resids(y_test, y_score)
+resid_qq(y_test, y_score)
+resid_plot(y_test, y_score)
+
+## Dump the model
+from joblib import dump
+dump(lin_mod, 'LinearModel_fs.joblib')
+
+
